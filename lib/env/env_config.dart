079ab0f6b1d0c1f934edc478d205ceb4206d854d
o-ifeanyi <onuifeanyi95@gmail.com>
2023-04-16 22:16:02 +0100

    fix test

--- lib/env/env_config.dart
+++ lib/env/env_config.dart
@@ -1,17 +1,13 @@
-import 'package:checklist_app/core/util/config.dart';
-import 'package:checklist_app/env/secrets.dart';
-
 abstract class BaseConfig {
   String get baseUrl;
 }
 
 class StagingEnv implements BaseConfig {
   @override
-  String get baseUrl =>
-      Config.isAndroid ? 'http://10.0.2.2:8080/' : 'http://localhost:8080/';
+  String get baseUrl => '';
 }
 
 class ProductionEnv implements BaseConfig {
   @override
-  String get baseUrl => '${Secrets.PROD_URL}';
+  String get baseUrl => '';
 }
